40a777ca64f57ae07999b9df5fe1676f6fdac1c1
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2024-11-08 16:32:39 +0000

    ajeitando formatacao

--- testeStrLen.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char nome[1000] = "textoooooo";
-    int tamanho = strlen(nome);
-    printf("O tamanho da string é %d\n", tamanho);
-    return 0;
-}
