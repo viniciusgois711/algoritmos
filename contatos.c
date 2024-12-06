9f89e777b88bdf4f6d9706f0ab90c25bd99a01df
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2024-12-06 11:38:51 +0000

    add função para listar contatos

--- contatos.c
+++ contatos.c
@@ -16,29 +16,40 @@ struct Contatos* criarContatos(char nome[], char telefone[]){
     return contato;
 };
 
-struct Contatos* listarContatos(){
-
-    
-
-}
+void listarContatos(struct Contatos* contato[], int tamanho){
+    if(tamanho==0){
+        printf("Nenhum contato cadastrado");
+    }else{
+        for(int i=0; i<tamanho; i++){
+            printf("\nContato:");
+            printf("\nNome: %s", contato[i]->nome); 
+            printf("\nTelefone: %s", contato[i]->telefone);
+        }
+    }
+};
 
 int main(){
     int op = 0;
     char nome[50];
     char telefone[15];
-    while(op!=0){
-        printf("\nMenu\n1-Adicionar contato\n2-Listar contatos\n3-Editar contato\n4-Excluir contato\n-1-sair");
+    int contador = 0;
+    struct Contatos* contatos[100]; 
+
+    while(op!=5){
+        printf("\nMenu\n1-Adicionar contato\n2-Listar contatos\n3-Editar contato\n4-Excluir contato\n5-sair:\n");
         scanf("%d", &op);
 
         switch(op){
             case 1:
                 printf("\nDigite o nome:\n");
                 scanf("%s", nome);
+                printf("\nDigite o telefone:\n");
                 scanf("%s", telefone);
-                struct Contatos* contato = criarContatos(nome, telefone);
+                contatos[contador] = criarContatos(nome, telefone);
+                contador++;
             break;
             case 2:
-
+                listarContatos(contatos, contador);
         }
     }
 }   
