433ccbcd3e327af72e697962ff37349684cf6eab
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2024-12-10 02:30:10 +0000

    q1 tp3

---
+++ tp3/q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+int main(){
+    int array[5];
+
+    for(int i =0; i<5; i++){
+        scanf("%d", &array[i]);
+    }
+
+    for(int i = 0; i<5; i++){
+        printf("%d\n", array[4-i]);
+    }
+
+}
