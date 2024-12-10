b312659436bcced436ee254f853702339e861c9a
Lucas Vinícius Teixeira de Góis <lu.viniteixeira78@gmail.com>
2024-12-10 02:43:18 +0000

    iniciando q2

---
+++ tp3/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char gabarito[10];
+    int qtdAlunos;
+
+    printf("Digite o gabarito\n");
+    for(int i = 0; i<10; i++){
+        scanf(" %c", &gabarito[i]);
+    }    
+
+    printf("Digite a quantidade de alunos");
+    scanf("%d", qtdAlunos);
+
+
+}
